acbd08c3b802a981b78c3898073a5079a4a4c191
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-13 20:12:00 +0100

    Add the stle for login card

--- src/routes/homeLogin/HomeLogin.jsx
+++ src/routes/homeLogin/HomeLogin.jsx
@@ -1,16 +1,60 @@
 import "./homeLogin.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPrescriptionBottleMedical, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 
 const HomeLogin = () => {
   return (
     <section className="login_container">
-      <h2>EasiPharma</h2>
+      <div className="login_hero">
+        <h2>EasiPharma</h2>
+        <FontAwesomeIcon icon={faPrescriptionBottleMedical} />
+      </div>
       <div className="login_wrapper">
         <div className="login_card">
-          
-            <h1>Welcome to login page</h1>
-          
+          <div>
+            <div className="icon">
+              <FontAwesomeIcon icon={faRightToBracket} />
+            </div>
+
+            <p>Sign in with your User ID and password</p>
+          </div>
+
+          <div>
+            <form action="#" method="get">
+              <fieldset>
+                <div>
+                  <input
+                    type="text"
+                    name="user_id"
+                    placeholder="User ID"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <input
+                    type="password"
+                    name="Pword"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+
+                <div className="drop_down">
+                  <label htmlFor="options"> Level:</label>
+                  <select name="level" id="options">
+                    <option value="option1">Admin: Manager</option>
+                    <option value="option2">Store Keeper</option>
+
+                    <option value="option3">Staff</option>
+                  </select>
+                </div>
+
+              <div className="btn"><button type="submit"> Sign In</button></div>
+              </fieldset>
+            </form>
+          </div>
         </div>
       </div>
     </section>
