5580f6be4bff076b18370c5db968e69ddcd5abf6
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-13 12:18:19 +0100

    Set the login ng-img

--- src/routes/homeLogin/HomeLogin.jsx
+++ src/routes/homeLogin/HomeLogin.jsx
@@ -1,6 +1,19 @@
+import "./homeLogin.css";
+
+
+
 const HomeLogin = () => {
   return (
-    <h1>Welcome to login page</h1>
-    )
+    <section className="login_container">
+      <h2>EasiPharma</h2>
+      <div className="login_wrapper">
+        <div className="login_card">
+          
+            <h1>Welcome to login page</h1>
+          
+        </div>
+      </div>
+    </section>
+  );
 }
 export default HomeLogin;
