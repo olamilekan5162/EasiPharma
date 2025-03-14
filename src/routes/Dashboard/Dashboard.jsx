6d9e4163dc9e709fb951c3ee70d746df1778d6bd
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-14 23:31:06 +0100

    Add the styling for the dashboard component

--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,22 +1,33 @@
 import "./dashboard.css";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import {
   AiTwotoneDashboard as Dashboardicon,
   AiOutlineStock as Stockicon,
 } from "react-icons/ai";
 import { FaShareSquare as Distribeicon } from "react-icons/fa";
-import { MdOutlineSupportAgent as Suppliericon } from "react-icons/md";
 import {
-  CiCircleList as Stafficon,
+  MdOutlineSupportAgent as Suppliericon,
+  MdInventory as Inventoryicon,
+} from "react-icons/md";
+import {
   CiSettings as Settingicon,
+  CiCalendarDate as Dateicon,
 } from "react-icons/ci";
+import {
+  IoPersonCircleSharp as Profileicon,
+  IoStorefront as Storeicon,
+  IoBagCheckOutline as Checkouticon
+} from "react-icons/io5";
+import { GrUserWorker as Stafficon, GrStatusGoodSmall as Pendingicon } from "react-icons/gr";
+import { GiMedicines as Drugicon } from "react-icons/gi";
+import { TbBuildingWarehouse as Warehouseicon } from "react-icons/tb";
 import Button from "../../components/ui/button/Button";
 
 const Dashboard = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleSignOut = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
   return (
     <section className="dashboard">
       <div className="sidebar">
@@ -55,14 +66,101 @@ const Dashboard = () => {
         </div>
 
         <div className="D_btn">
-          <Button variant="primary" onClick={handleSignOut}>
-        Sign Out 
-      </Button>
+          <Button variant="secondary" onClick={handleSignOut}>
+            Sign Out
+          </Button>
         </div>
       </div>
 
-      <div className="mainbar">
-        <h1>Second grid</h1>
+      <div className="D_main">
+        <div>
+          <div className="D_navbar">
+            <div className="D_navbar_card">
+              <Profileicon className="D_navbar_icon" />
+              <h1 className="D_navbar_text">Admin: Manager</h1>
+            </div>
+            <div className="D_navbar_card">
+              <Dateicon className="D_navbar_icon" />
+              <h1 className="D_navbar_text">
+                <i>Date</i>
+              </h1>
+            </div>
+            <div>
+              <Settingicon className="D_navbar_set" />
+            </div>
+          </div>
+
+          <div className="warning_box">
+            <p>warning</p>
+          </div>
+        </div>
+
+        {/* Dashboard component, copy from here */}
+        <div className="D_mainbar">
+          <div className="D_mainbar_card">
+            <span>
+              <Storeicon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Total Stocks</p>
+            <p>100</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Checkouticon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Last Order</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Pendingicon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Pending Oder</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Warehouseicon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Check Stocks</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Inventoryicon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Check Inventory</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Stafficon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Staff list</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Suppliericon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Add Supplier</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Distribeicon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Distribution Records</p>
+          </div>
+
+          <div className="D_mainbar_card">
+            <span>
+              <Drugicon className="D_mainbar_icon" />
+            </span>
+            <p className="D_mainbar_text">Order Stocks</p>
+          </div>
+        </div>
       </div>
     </section>
   );
