f7bbf45d89a21d3970f524996b1df7d042532d63
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-14 12:35:42 +0100

    Add sign out button to dashboard

--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,42 +1,58 @@
 import "./dashboard.css";
-import { AiTwotoneDashboard } from "react-icons/ai";
-
+import {
+  AiTwotoneDashboard as Dashboardicon,
+  AiOutlineStock as Stockicon,
+} from "react-icons/ai";
+import { FaShareSquare as Distribeicon } from "react-icons/fa";
+import { MdOutlineSupportAgent as Suppliericon } from "react-icons/md";
+import {
+  CiCircleList as Stafficon,
+  CiSettings as Settingicon,
+} from "react-icons/ci";
 
 const Dashboard = () => {
   return (
     <section className="dashboard">
       <div className="sidebar">
         <div className="dashboard_hero">
-          <h1 className="D_hero_text">EasiPharma</h1>
+          <h1 className="D_hero_text">EasiPharma Management Ltd</h1>
         </div>
 
         <div className="boxes">
-          <div className="sidebox">
-            <h1 className="sidetext">Dashboard</h1>
-            <AiTwotoneDashboard/>
+          <div className="sidebox active">
+            <Dashboardicon className="icon active" />
+            <h1 className="sidetext active">Dashboard</h1>
           </div>
           <div className="sidebox">
+            <Stockicon className="icon" />
             <h1 className="sidetext">Stocks</h1>
-            </div>
+          </div>
           <div className="sidebox">
+            <Distribeicon className="icon" />
             <h1 className="sidetext">Distribution</h1>
           </div>
           <div className="sidebox">
+            <Suppliericon className="icon" />
             <h1 className="sidetext">Suppliers</h1>
           </div>
           <div className="sidebox">
+            <Stafficon className="icon" />
             <h1 className="sidetext">Staff list</h1>
           </div>
-      </div>
+        </div>
 
-      <div className="sidebox">
-        <h1 className="sidetext">Settings</h1>
-      </div>
+        <div className="setbox">
+          <div className="sidebox">
+            <Settingicon className="icon" />
+            <h1 className="sidetext">Settings</h1>
+          </div>
+        </div>
 
-
-      <div className="D_btn">
-        <a href="#"><button>Sign Out</button></a>
-      </div>
+        <div className="D_btn">
+          <a href="#">
+            <button>Sign Out</button>
+          </a>
+        </div>
       </div>
 
       <div className="mainbar">
