9e439c9aa2e0a2ac3187c3465e4a65a9dbba3ca7
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-14 21:39:27 +0100

    resolve the login icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
-import Stocks from "./routes/Stocks/Stocks"
 import Dashboard from "./routes/Dashboard/Dashboard"
 import HomeLogin from "./routes/homeLogin/HomeLogin"
 
