36bfa8df6d6880c87716cdd8a71672a88c026bdb
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-16 20:54:40 +0100

    Remove staff list from side bar

--- src/components/ui/staffList/StaffList.jsx
+++
@@ -1,6 +0,0 @@
-const StaffList = () => {
-  return (
-    <h1>StaffList</h1>
-    )
-}
-export default StaffList;
--- src/components/ui/suppliers/Suppliers.jsx
+++ src/components/ui/suppliers/Suppliers.jsx
@@ -1,6 +1,19 @@
+import "./supplier.css"
+import {
+  FaPlus as Plusicon,
+  FaRegSquare as Squareicon,
+  FaEdit as Editicon,
+} from "react-icons/fa";
+
 const Suppliers = () => {
   return (
-    <h1>Suppliers</h1>
-    )
-}
+    <section className="supplier_page">
+      <div className="supplier_card">
+        <div className="add_supplier">
+          <h1>Add supplier</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Suppliers;
