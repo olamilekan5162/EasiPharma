87ac30bd90f9c9cc5796af167351a5c4be50c012
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-03-20 19:32:07 +0100

    fix unpersistence user and level context

--- src/utils/UserAuthContext.jsx
+++ src/utils/UserAuthContext.jsx
@@ -1,10 +1,25 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [level, setLevel] = useState(null)
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const [level, setLevel] = useState(() => {
+    const storedLevel = localStorage.getItem('level');
+    return storedLevel ? JSON.parse(storedLevel) : null;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
+  useEffect(() => {
+    localStorage.setItem('level', JSON.stringify(level));
+  }, [level]);
 
   return (
     <UserContext.Provider value={{ user, level, setLevel, setUser }}>
